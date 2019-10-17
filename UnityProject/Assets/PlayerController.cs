f697e197467bf99daeae8bdb649c6bb244db43ba
l9811598117iu <55218520+l9811598117iu@users.noreply.github.com>
2019-10-17 11:59:34 +0800

    KID_HomeWork_1017_project_B1040437
    Kid_project_B1040437

---
+++ UnityProject/Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Animator Anim;
+
+	// Use this for initialization
+	void Start () {
+        Anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Anim.SetTrigger("吃飯觸發器");
+        }
+        if (Input.GetKey(KeyCode.R))
+        {
+            Anim.SetBool("跑步開關", true);
+        }
+        else
+        {
+            Anim.SetBool("跑步開關", false);
+        }
+    }
+}
